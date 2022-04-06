faef06de91f7d6b033ab6b92018d730d57bb90b0
kushalkhare.official@gmail.com <kushalkhare.official@gmail.com>
2022-04-06 22:57:30 +0530

    setup analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 import {
   GoogleAuthProvider,
   getAuth,
@@ -7,7 +8,7 @@ import {
   createUserWithEmailAndPassword,
   sendPasswordResetEmail,
   signOut,
-} from "firebase/auth";
+} from 'firebase/auth'
 import {
   getFirestore,
   query,
@@ -15,83 +16,85 @@ import {
   collection,
   where,
   addDoc,
-} from "firebase/firestore";
+} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRA-fNYF6rI8YyVJU5yQj7QXlNecZiiQg",
-  authDomain: "tech-novation.firebaseapp.com",
-  databaseURL: "https://tech-novation.firebaseio.com",
-  projectId: "tech-novation",
-  storageBucket: "tech-novation.appspot.com",
-  messagingSenderId: "323281047271",
-  appId: "1:323281047271:web:56cbaea62ab3eb9e875c42",
-  measurementId: "G-6YGL2B90YP",
-};
+  apiKey: 'AIzaSyCRA-fNYF6rI8YyVJU5yQj7QXlNecZiiQg',
+  authDomain: 'tech-novation.firebaseapp.com',
+  databaseURL: 'https://tech-novation.firebaseio.com',
+  projectId: 'tech-novation',
+  storageBucket: 'tech-novation.appspot.com',
+  messagingSenderId: '323281047271',
+  appId: '1:323281047271:web:56cbaea62ab3eb9e875c42',
+  measurementId: 'G-6YGL2B90YP',
+}
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const googleProvider = new GoogleAuthProvider();
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const auth = getAuth(app)
+const db = getFirestore(app)
+const googleProvider = new GoogleAuthProvider()
 
 const signInWithGoogle = async () => {
   try {
-    const res = await signInWithPopup(auth, googleProvider);
-    const user = res.user;
-    const q = query(collection(db, "users"), where("uid", "==", user.uid));
-    const docs = await getDocs(q);
+    const res = await signInWithPopup(auth, googleProvider)
+    const user = res.user
+    const q = query(collection(db, 'users'), where('uid', '==', user.uid))
+    const docs = await getDocs(q)
     if (docs.docs.length === 0) {
-      await addDoc(collection(db, "users"), {
+      await addDoc(collection(db, 'users'), {
         uid: user.uid,
         name: user.displayName,
-        authProvider: "google",
+        authProvider: 'google',
         email: user.email,
-      });
+      })
     }
   } catch (err) {
-    console.error(err);
-    alert(err.message);
+    console.error(err)
+    alert(err.message)
   }
-};
+}
 const logInWithEmailAndPassword = async (email, password) => {
   try {
-    await signInWithEmailAndPassword(auth, email, password);
+    await signInWithEmailAndPassword(auth, email, password)
   } catch (err) {
-    console.error(err);
-    alert(err.message);
+    console.error(err)
+    alert(err.message)
   }
-};
+}
 
 const registerWithEmailAndPassword = async (name, email, password) => {
   try {
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-    const user = res.user;
-    await addDoc(collection(db, "users"), {
+    const res = await createUserWithEmailAndPassword(auth, email, password)
+    const user = res.user
+    await addDoc(collection(db, 'users'), {
       uid: user.uid,
       name,
-      authProvider: "local",
+      authProvider: 'local',
       email,
-    });
+    })
   } catch (err) {
-    console.error(err);
-    alert(err.message);
+    console.error(err)
+    alert(err.message)
   }
-};
+}
 
 const sendPasswordReset = async (email) => {
   try {
-    await sendPasswordResetEmail(auth, email);
-    alert("Password reset link sent!");
+    await sendPasswordResetEmail(auth, email)
+    alert('Password reset link sent!')
   } catch (err) {
-    console.error(err);
-    alert(err.message);
+    console.error(err)
+    alert(err.message)
   }
-};
+}
 
 const logout = () => {
-  signOut(auth);
-};
+  signOut(auth)
+}
 
 export {
+  analytics,
   auth,
   db,
   signInWithGoogle,
@@ -99,4 +102,4 @@ export {
   registerWithEmailAndPassword,
   sendPasswordReset,
   logout,
-};
+}
