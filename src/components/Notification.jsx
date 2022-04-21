daf85b847abdf4b1bfff933e787729be4bf6cce1
kushalkhare.official@gmail.com <kushalkhare.official@gmail.com>
2022-04-22 01:00:41 +0530

    minor bug fix

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,56 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 import {
   Box,
   Accordion,
   AccordionSummary,
   AccordionDetails,
   Typography,
-} from "@mui/material";
-import { ExpandMore } from "@mui/icons-material";
-import { Link } from "react-router-dom";
-import Loader from "./Loader";
+} from '@mui/material'
+import { ExpandMore } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+import Loader from './Loader'
 const Notifications = () => {
-  const [notifications, setNotification] = useState([]);
-  const [Loading, setLoading] = useState(true);
+  const [notifications, setNotification] = useState([])
+  const [Loading, setLoading] = useState(true)
   useEffect(async () => {
     const response = await fetch(
-      process.env.REACT_APP_BACKEND_URL + "/notify",
+      process.env.REACT_APP_BACKEND_URL + '/notify',
       {
-        method: "GET",
+        method: 'GET',
       }
-    );
-    const data = await response.json();
-    setNotification(data);
-    console.log(notifications);
-    setLoading(false);
-  }, [Loading]);
+    )
+    const data = await response.json()
+    setNotification(data)
+    console.log(notifications)
+    setLoading(false)
+  }, [Loading])
   return (
     <>
       <div className="login-box">
-        <h3 style={{ textAlign: "center" }}>Notice</h3>
+        <h3 style={{ textAlign: 'center' }}>Notice</h3>
         {Loading ? (
           <Loader />
         ) : (
           <>
             {notifications.map((notification) => (
-              <div key={notification._id} style={{ marginBottom: "0.7em" }}>
-                <Accordion sx={{ background: "#200C2C", color: "white" }}>
+              <div key={notification._id} style={{ marginBottom: '0.7em' }}>
+                <Accordion sx={{ background: '#200C2C', color: 'white' }}>
                   <AccordionSummary
                     expandIcon={<ExpandMore />}
                     aria-controls="panel1a-content"
                     id="panel1a-header"
                     sx={{
-                      "& div": { justifyContent: "space-between" },
-                      "& svg": { color: "white" },
+                      '& div': { justifyContent: 'space-between' },
+                      '& svg': { color: 'white' },
                     }}
                   >
                     <Typography
                       variant="h6"
                       sx={{
-                        textOverflow: "ellipsis",
+                        textOverflow: 'ellipsis',
                         fontFamily: "'Nunito', sans-serif",
-                        fontSize: "1.15rem",
-                        fontWeight: "300",
+                        fontSize: '1.15rem',
+                        fontWeight: '300',
                       }}
                     >
                       {notification.title}
@@ -58,10 +58,10 @@ const Notifications = () => {
                     <Typography component="div" align="right">
                       <Box
                         sx={{
-                          alignSelf: "end",
-                          fontSize: "0.8em",
-                          fonttextalign: "right",
-                          fontStyle: "italic",
+                          alignSelf: 'end',
+                          fontSize: '0.8em',
+                          fonttextalign: 'right',
+                          fontStyle: 'italic',
                           m: 1,
                         }}
                       >
@@ -70,22 +70,26 @@ const Notifications = () => {
                         ).getDay()
                           ? new Date(
                               Date.now() - new Date(notification.createdAt)
-                            ).getDay() + " Days Ago"
+                            ).getDay() + ' Days Ago'
                           : new Date(
                               Date.now() - new Date(notification.createdAt)
-                            ).getHours() + " Hours Ago"}
+                            ).getHours() + ' Hours Ago'}
                       </Box>
                     </Typography>
                   </AccordionSummary>
                   <AccordionDetails>
-                    <Typography variant="h6" component="span">
+                    <Typography
+                      variant="h6"
+                      component="span"
+                      style={{ overflowWrap: 'anywhere' }}
+                    >
                       {notification.description}
                       <br></br>
                       <ul>
                         {notification.links.map((link, i) => (
                           <li key={i}>
-                            <a href={link} style={{ color: "wheat" }}>
-                              link
+                            <a href={link} style={{ color: 'wheat' }}>
+                              {link}
                             </a>
                           </li>
                         ))}
@@ -99,7 +103,7 @@ const Notifications = () => {
         )}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Notifications;
+export default Notifications
